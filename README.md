88b1a0357e18bd0174d07f32029c49134d87e933
Danny van der Meulen <danny@cb750k6.nl>
2015-08-25 12:06:44 +0200

    More usefull as a cron job

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # esstats
 Elasticsearch stats for graphite, this is a WIP. Works but needs a bit of work.
-Should run under supervision of supervisor or upstart.
+Should run as a cron job on the master nodes.
 
 ```
 usage: es_stats.py [-h] [--es-hosts ES_HOSTS] [--carbon-server CARBON_SERVER]
-                   [--carbon-port CARBON_PORT] [--interval INTERVAL]
-                   [--state-index STATE_INDEX] [--verbose]
+                   [--carbon-port CARBON_PORT] [--state-index STATE_INDEX]
+                   [--lock-ttl LOCK_TTL] [--verbose]
 
 optional arguments:
   -h, --help            show this help message and exit
@@ -15,9 +15,9 @@ optional arguments:
                         Carbon server
   --carbon-port CARBON_PORT, -p CARBON_PORT
                         Corbon port
-  --interval INTERVAL, -i INTERVAL
-                        Check interval in seconds
   --state-index STATE_INDEX, -s STATE_INDEX
-                        Index to use for keeping state
+                        Index to use for keeping a lock
+  --lock-ttl LOCK_TTL, -t LOCK_TTL
+                        Ttl for lock in elasticsearch
   --verbose, -v         Be verbose, useful for debugging
 ```
--- es_stats.py
+++ es_stats.py
@@ -3,42 +3,88 @@
 import time
 import socket
 import logging
+import sys
 from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import NotFoundError
 from collections import MutableMapping
 from re import sub
 from argparse import ArgumentParser
 
-parser = ArgumentParser()
-parser.add_argument('--es-hosts', '-e', help="Elasticsearch master nodes", default="localhost")
-parser.add_argument('--carbon-server', '-c', help="Carbon server", default="localhost")
-parser.add_argument('--carbon-port', '-p', help="Corbon port", type=int, default="2003")
-parser.add_argument('--interval', '-i', help="Check interval in seconds", type=int, default=10)
-parser.add_argument('--state-index', '-s', help="Index to use for keeping state", default="state_index")
-parser.add_argument('--verbose', '-v', help="Be verbose, useful for debugging", action="store_true")
+def parse():
+    """Parse commandline arguments"""
+    parser = ArgumentParser()
+    parser.add_argument('--es-hosts', '-e', help="Elasticsearch master nodes", default="localhost")
+    parser.add_argument('--carbon-server', '-c', help="Carbon server", default="localhost")
+    parser.add_argument('--carbon-port', '-p', help="Corbon port", type=int, default="2003")
+    parser.add_argument('--state-index', '-s', help="Index to use for keeping a lock", default=".es_stats_lock")
+    parser.add_argument('--lock-ttl', '-t', help="Ttl for lock in elasticsearch", default="120s")
+    parser.add_argument('--verbose', '-v', help="Be verbose, useful for debugging", action="store_true")
 
-log = logging.getLogger('elasticsearch-graphite')
+    return parser.parse_args()
 
-def flatten(d, parent_key='', sep='.'):
+
+def runnable(es, index, lock_ttl):
+    """Check if no other master has the lock set"""
+    res = ''
+    runnable = False
+    index_mapping = {
+            "mappings" : {
+                "lock" : {
+                    "_ttl" : {
+                        "enabled" : True
+                        }
+                    }
+                }
+            }
+
+    doc = { "active_host" : socket.gethostname() }
+
+    if not es.indices.exists(index=index):
+        print "Creating index"
+        es.indices.create(index=index, body=index_mapping)
+
+    try:
+        res = es.get(index=index, doc_type='lock', id=1)
+    except NotFoundError:
+        es.create(index=index, doc_type='lock', id=1, ttl=lock_ttl, body=doc)
+        res = es.get(index=index, doc_type='lock', id=1)
+    except:
+        print "Unexpected error: sys.exc_info()[0]"
+        raise
+
+    if res['_source']['active_host'] == socket.gethostname():
+        es.index(index=index, doc_type='lock', id=1, ttl=lock_ttl, body=doc)
+        runnable = True
+
+    return runnable
+
+
+def format4graphite(d, parent_key='', sep='.'):
     """Flatten dicts when nested"""
     items = []
     for key, v in d.items():
         k = sub('\.', '_', key)
         new_key = parent_key + sep + k if parent_key else k
         if isinstance(v, MutableMapping):
-            items.extend(flatten(v, new_key, sep=sep).items())
+            items.extend(format4graphite(v, new_key, sep=sep).items())
         else:
             items.append((new_key, v))
 
     return dict(items)
 
+
 def main():
-    args = parser.parse_args()
+    args = parse()
+    log = logging.getLogger('elasticsearch-graphite')
     es = Elasticsearch(args.es_hosts.split(','))
 
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
 
-    while True:
+    if not runnable(es, args.state_index, args.lock_ttl):
+        log.debug('Not runnable, bailing out')
+        sys.exit(0)
+    else:
         sock = socket.socket()
         sock.settimeout(5.0)
 
@@ -46,39 +92,32 @@ def main():
             sock.connect((args.carbon_server, args.carbon_port))
         except:
             log.debug("Could not connect to graphite on {}:{}".format(args.carbon_server, args.carbon_port))
-            time.sleep(2)
-            continue
+            raise
 
-        while True:
-            node_stats = es.nodes.stats()
-            cluster_name = node_stats['cluster_name']
-            named_node_stats = {}
+        node_stats = es.nodes.stats()
+        cluster_name = node_stats['cluster_name']
+        named_node_stats = {}
 
-            for k, v in node_stats['nodes'].iteritems():
-                node_name = node_stats['nodes'][k]['name']
-                named_node_stats[node_name] = v
+        for k, v in node_stats['nodes'].iteritems():
+            node_name = node_stats['nodes'][k]['name']
+            named_node_stats[node_name] = v
 
-            stats = [
-                flatten(named_node_stats),
-                flatten(es.cluster.stats()),
-                flatten(es.indices.stats())
-                ]
+        stats = [
+            format4graphite(named_node_stats),
+            format4graphite(es.cluster.stats()),
+            format4graphite(es.indices.stats())
+            ]
 
-            for d in stats:
-                for k, v in d.iteritems():
-                    if isinstance(v, int):
-                        metrics = 'elasticsearch.{}.{} {} {}'.format(cluster_name, k, v, int(time.time()))
-                        log.debug(metrics)
-                        try:
-                            sock.sendall(metrics + '\n')
-                        except:
-                            log.debug("Could not send metrics...")
-                            break
-
-            else:
-                time.sleep(args.interval)
-                continue
-            break
+        for d in stats:
+            for k, v in d.iteritems():
+                if isinstance(v, int):
+                    metrics = 'elasticsearch.{}.{} {} {}'.format(cluster_name, k, v, int(time.time()))
+                    log.debug(metrics)
+                    try:
+                        sock.sendall(metrics + '\n')
+                    except:
+                        log.debug("Could not send metrics...")
+                        sys.exit(1)
 
 if __name__ == '__main__':
     main()
